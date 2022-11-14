e4af047ba1810cd476f4a85abb8e92ff438064fa
정혜주 <fixup719@gmail.com>
2022-11-14 17:44:55 +0900

    1114

---
+++ 1114_live/vuex-test/src/components/step01/SubjectComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="addCount">{{ title }} - {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: "SubjectComponent",
+  props: ["title"],
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addCount: function () {
+      this.count += 1;
+      // this.$emit("addtotcount"); 부모한테 안 보냄
+      this.$store.state.count++; // this.$store => store/index.js
+    },
+  },
+};
+</script>
+
+<style></style>
